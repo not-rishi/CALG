0b0cb41f97b6c1764d5f552365784b03247e4275
Not Rishi <182108487+not-rishi@users.noreply.github.com>
2026-05-04 17:40:52 +0530

    Change Router from BrowserRouter to HashRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import { AppProvider } from "./context/AppContext";
 import GPACalculator from "./pages/GPACalculator";
 import Guide from "./pages/Guide";
